06ba89178ed1eb4c1cc815a548cb50613a7f9bc1
andre <abizetto@ciandt.com>
2018-10-23 14:27:36 -0300

    Project 2: event Listeners

--- androidApp2/app/src/main/java/com/google/androidapp2/MainActivity.java
+++ androidApp2/app/src/main/java/com/google/androidapp2/MainActivity.java
@@ -3,6 +3,10 @@ package com.google.androidapp2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,12 +14,53 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "andreMessage";
     static final String STATE_SCORE = "playerScore";
     static final String STATE_LEVEL = "playerLevel";
+    private int ClickCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
+
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(
+
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        ClickCount+=1;
+
+                        TextView lblResult = (TextView)findViewById(R.id.lblResult);
+                        lblResult.setText("Click Action: " + ClickCount);
+
+                    }
+                }
+
+        );
+
+        btnStart.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    @Override
+                    public boolean onLongClick(View v) {
+                        TextView lblResult = (TextView)findViewById(R.id.lblResult);
+                        lblResult.setText("Looong Click Action....");
+
+                        //Onclick will be triggered
+                        //return false;
+
+                        //Onclick will NOT be triggered
+                        return true;
+                    }
+                }
+
+        );
+
+
+
+
+
+
+
     }
 
     @Override
