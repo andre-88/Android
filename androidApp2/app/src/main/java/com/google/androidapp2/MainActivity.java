55f45c4cf1f0ab3a98145b618a5aeeb730668d54
andre <abizetto@ciandt.com>
2018-10-23 10:13:37 -0300

    Project 2: lifecycle and logs

---
+++ androidApp2/app/src/main/java/com/google/androidapp2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.google.androidapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "andreMessage";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+}
